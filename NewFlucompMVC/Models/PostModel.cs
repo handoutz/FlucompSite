86dd56240849864d68cf0707b50a95d05d1a2a96
Vince <innatepirate@gmail.com>
2013-04-05 13:49:34 -0500

    started working on a blog, updated home page <p> tag now has a margin of 0.25em on the bottom grid layout start

---
+++ NewFlucompMVC/Models/PostModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewFlucompMVC.Models
+{
+    public class PostModel
+    {
+        public string Title { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Author { get; set; }
+        public string Contents { get; set; }
+    }
+}
